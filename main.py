6c4c25f281e3e10bcf4143e53be4858d9924552f
Peter Taraba <taraba.pepe@gmail.com>
2020-12-19 19:51:23 -0500

    .

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def log_sm_loss(input, target):
 # standard deviation loss
 # works pretty well, one run gave me 95.35 % accuracy [95.3 - 95.8 % for log_sm_loss]
 def std_loss(input, target):
-    # here is bit of cheating - I ask for averages to be -1 (incorrect) and 9 correct
+    # here is bit of cheating - I ask for outputs to be -1 for incorrect and 9 for correct cases
     # could beverified if other values work as well or
     tmp = torch.ones(input.shape, requires_grad=False) * (-1.0)
     for i in range(input.shape[0]):
