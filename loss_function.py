00fca1d16fe007193cb840ef0f57fc66f70fb3b7
Peter Taraba <taraba.peter@mail.com>
2020-12-23 17:45:53 -0500

    forgot smooth entropy needed for sam

--- loss_function.py
+++ loss_function.py
@@ -28,6 +28,16 @@ def log_sm_loss(input, target):
     return loss.sum() / target.shape[0]
 
 
+def smooth_crossentropy(pred, gold, smoothing=0.1):
+    n_class = pred.size(1)
+
+    one_hot = torch.full_like(pred, fill_value=smoothing / (n_class - 1))
+    one_hot.scatter_(dim=1, index=gold.unsqueeze(1), value=1.0 - smoothing)
+    log_prob = F.log_softmax(pred, dim=1)
+
+    return F.kl_div(input=log_prob, target=one_hot, reduction='none').sum(-1)
+
+
 # standard deviation loss
 # works pretty well, one run gave me 95.35 % accuracy [95.2 - 95.8 % for log_sm_loss]
 # second run 95.34 % accuracy
