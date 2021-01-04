b5a619c45e1acbdbec34fd3dd544d21a330d8b28
Peter Taraba <taraba.peter@mail.com>
2021-01-03 20:21:08 -0500

    clean up

--- loss_function.py
+++ loss_function.py
@@ -9,9 +9,6 @@ import torchvision.transforms as transforms
 import math
 
 
-# loss, which is minus log of main (target) probability
-# pytorch calls it cross entropy loss
-# gives same results as their's - tested
 def log_sm_loss(input, target):
     eout = torch.exp(input)
     seout = torch.sum(eout,dim=1)
@@ -36,23 +33,12 @@ def smooth_crossentropy(pred, gold, smoothing=0.1):
     return F.kl_div(input=log_prob, target=one_hot, reduction='none').sum(-1)
 
 
-# standard deviation loss
-# works pretty well, one run gave me 95.35 % accuracy [95.2 - 95.8 % for log_sm_loss]
-# second run 95.34 % accuracy
-# third run 95.19 % accuracy
-# optimizing power std^0.5 - one run of 95.50 % (makes it quite competitive with cross entropy)
-# optimizing power std^0.1 - one run of 95.18 %
 def std_loss(input, target):
-    # here is bit of cheating - I ask for outputs to be -0.5 for incorrect and 0.5 for correct cases
-    tmp = torch.ones(input.shape, requires_grad=False) * (-0.5)
-    for i in range(input.shape[0]):
-        tmp[i,target[i]] = 0.5
+    tmp = torch.full_like(input, fill_value = -0.5)
+    tmp.scatter_(dim = 1, index = target.unsqueeze(1), value = 0.5)
+    dif = input - tmp
+    dif2 = dif * dif
+    adif2 = dif2.sum() / (input.shape[0] * input.shape[1] - 1.0)
+    std = torch.sqrt(adif2)
     
-    tmp = tmp.to('cuda')
-    tmp = input - tmp
-    tmp2 = tmp * tmp
-    tmp3 = tmp2.sum() / (input.shape[0] * input.shape[1] - 1.0)
-    tmp4 = torch.sqrt(tmp3)
-    
-    return tmp4
-
+    return std
